3dbb7fb03dfe4cffb83d9aa47be6fb67e8cedae8
DevTheDan <63319331+DevTheDan@users.noreply.github.com>
2021-12-18 00:58:55 +0300

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  speed: 1300,
+  autoHeight: true,
+
+  navigation: {
+    nextEl: '.swiper-btn-next',
+    prevEl: '.swiper-btn-prev',
+  }
+});
+
